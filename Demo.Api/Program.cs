f302c8eaa3283748ad4d0609dac4cc1da00c605e
necati han <han.necati@gmail.com>
2024-03-18 11:42:38 +0100

    first commit

---
+++ Demo.Api/Program.cs
@@ -0,0 +1,8 @@
+﻿using FastEndpoints;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder();
+builder.Services.AddFastEndpoints();
+
+WebApplication app = builder.Build();
+app.UseFastEndpoints();
+app.Run();
